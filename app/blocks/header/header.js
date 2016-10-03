de45d03a64ed867d7873d20376f5ec4ae487aa1e
David Samper <davidm@memorandum.net>
2016-10-03 12:17:07 +0200

    Correccion de funcionalidad de cabecera

--- app/blocks/header/header.js
+++ app/blocks/header/header.js
@@ -1,22 +1,61 @@
 (function(global){
 	var header = {};
 
+	// jQuery solution
 	header.init = function init(){
 		var headerElement = $("header.header"),
     headerFixedClass = "header_fixed",
     hero = $('.hero'),
-    heroTop = hero.offset().top,
-    headerHeight = $('header').height();
+    heroMarginTop = 60,
+    headerHeight = $('header').outerHeight();
 
 		$(window).scroll(function() {
 		  if( $(this).scrollTop() > headerHeight ) {
 		    headerElement.addClass(headerFixedClass);
-		    hero.css('marginTop', heroTop);
+		    hero.css('marginTop', headerHeight + heroMarginTop);
 		  } else {
 		    headerElement.removeClass(headerFixedClass);
-		    hero.css('marginTop', 60);
+		    hero.css('marginTop', heroMarginTop);
 		  }
 		});
+
+	/*var myHeader = document.getElementsByTagName('header')[0],
+			headerHeight = myHeader.getBoundingClientRect().height,
+			latestKnownScrollY = 0,
+			content = document.getElementById('mainContent'),
+			applied = false,
+			ticking = false;
+
+	function onScroll(){
+		latestKnownScrollY = window.scrollY || window.pageYOffset;
+		requestTick();
+	}
+	function requestTick(){
+		if(!ticking) {
+			requestAnimationFrame(update);
+		}
+		ticking = true;
+	}
+	function update(){
+		ticking = false;
+		var currentScroll = latestKnownScrollY;
+
+		if((currentScroll > headerHeight) && (!applied)) {
+			utils.addClass(myHeader, 'header_fixed');
+			content.style.paddingTop = currentScroll + "px";
+			applied = true;
+		}
+		if((currentScroll == 0) && applied) {
+			utils.removeClass(myHeader, 'header_fixed');
+			content.style.paddingTop = 0;
+			applied = false;
+		}
+	}
+
+	header.init = function init(){
+		window.addEventListener('scroll', onScroll, false);
+	}*/
 	}
 	global.header = header;
+
 })(this);
