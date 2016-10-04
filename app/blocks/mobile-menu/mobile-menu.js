70b3ebb4bab28ea6b8bc955ec016874113ee5f74
David Mrnustik <david.mrnustik@hotmail.com>
2016-10-04 23:58:28 +0200

    Cambio de menu icon

--- app/blocks/mobile-menu/mobile-menu.js
+++ app/blocks/mobile-menu/mobile-menu.js
@@ -2,26 +2,36 @@
 	var openMenu = {};
 
 	function toggleMenu() {
-		var mainContent = document.getElementById('mainContent');
-		var mobileMenu = document.getElementById('mobileMenu');
+		//var mainContent = document.getElementById('mainContent');
+		var mobileMenu = document.getElementById('mobileMenu'),
+				menuIcon = document.querySelector('.mobile-menu__menu-icon'),
+				header = document.querySelector('.header');
 
-		utils.toggleClass(mainContent, 'menu-open');
+		//utils.toggleClass(mainContent, 'menu-open');
 		utils.toggleClass(mobileMenu, 'mobile-menu_show');
 		utils.toggleClass(document.documentElement, 'menu-open');
+
+		if(!utils.hasClass(header, 'header_fixed')){
+			menuIcon.style.marginTop = "20px";
+		} else {
+			menuIcon.style.marginTop = "0px";
+		}
 	}
 
 	function eventListener(el, func) { // this function fixes bug for IE8
-		var elem = document.getElementById(el);
+		var elem = document.querySelectorAll(el);
 		
-		if (elem.addEventListener){
-			elem.addEventListener('click', func);
-		} else {
-			elem.attachEvent('click', func);
+		for (var i = 0, l = elem.length; i < l; i++) {
+			if (elem[i].addEventListener){
+				elem[i].addEventListener('click', func);
+			} else {
+				elem[i].attachEvent('click', func);
+			}
 		}
 	}
 
 	openMenu.init = function init() {
-		document.getElementById('mobileMenu') !== null && eventListener('menu', toggleMenu);
+		document.getElementById('mobileMenu') !== null && eventListener('.menu-icon', toggleMenu);
 	};
 
 	global.openMenu = openMenu;
