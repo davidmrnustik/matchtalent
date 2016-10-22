32a624a49cad54070a3c22c7be02c10d885c30f8
David Mrnustik <david.mrnustik@hotmail.com>
2016-10-22 23:06:25 +0200

    Space intentation instead

--- app/app.js
+++ app/app.js
@@ -1,21 +1,21 @@
 (function() {
-	openMenu.init();
-	cookies.init();
-	slider.init();
-	footerLinks.init();
-	header.init();
-	sliderImage.init();
-	ourPlans.init();
-	contact.init();
-	modal.init();
-	smoothScroll.init({
-	  selector: '[data-scroll]',
-	  selectorHeader: '[data-scroll-header]',
-	  speed: 1000,
-	  easing: 'easeInOutQuad',
-	  offset: 100,
-	  updateURL: true,
-	  callback: function ( anchor, toggle ) {}
-	});
-	intro.init();
+  openMenu.init();
+  cookies.init();
+  slider.init();
+  footerLinks.init();
+  header.init();
+  sliderImage.init();
+  ourPlans.init();
+  contact.init();
+  modal.init();
+  smoothScroll.init({
+    selector: '[data-scroll]',
+    selectorHeader: '[data-scroll-header]',
+    speed: 1000,
+    easing: 'easeInOutQuad',
+    offset: 100,
+    updateURL: true,
+    callback: function ( anchor, toggle ) {}
+  });
+  intro.init();
 })();
