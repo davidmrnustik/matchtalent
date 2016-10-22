1ffc5d1ece6dcc66a7bd932975df4cfd79bbe907
David Mrnustik <david.mrnustik@hotmail.com>
2016-10-22 20:08:26 +0200

    Updated deploy

--- deploy/deploy.rb
+++ deploy/deploy.rb
@@ -12,8 +12,8 @@ class Deploy
 	# Servidores FTP - array[server, user, pwd]
 	# 0 - demo, 1 - production
 	@servers = [
-		#["matchtalent.com.es", "webtalentdemo", "Hm69tu4~"],
-		["matchtalent.tests-mm.com", "ftp_matchtalent", "Icp6w2#5"],
+		["matchtalent.com.es", "webtalentdemo", "Hm69tu4~"],
+		#["matchtalent.tests-mm.com", "ftp_matchtalent", "Icp6w2#5"],
 		["matchtalent.com.es", "webtalent", "Hm69tu4~"]
 	]
 
@@ -34,18 +34,32 @@ class Deploy
 		puts "-------------------------"
 		ftp(@env)
 	end
-
+	
 	def self.ftp(env)
-		dir = Dir.glob(PATH_FINAL + "**/*").reject {|fn| File.directory?(fn)}
+		#dir = Dir.glob(PATH_FINAL + "**/*").reject {|fn| File.directory?(fn)}
+		dir = Dir.glob(PATH_FINAL + "**/*")
 		Net::FTP.open(@url_server[0]) do |ftp|
 			ftp.login(@url_server[1], @url_server[2])
 			ftp.passive = true
 			ftp.debug_mode = true
-			ftp.chdir('/')
 			dir.each do |file|
+				ftp.chdir('/')
 				index = file.split("/").index(DIR_BUILD)
-				#puts file.split("/").drop(index + 1).join("/")
-				ftp.put(file, file.split("/").drop(index + 1).join("/"))
+				output = file.split("/").drop(index + 1)
+				if File.directory?(file)
+					i = 0
+					if output.length > 1
+						while i < output.length - 1
+							ftp.chdir(output[i])
+							i+=1
+						end
+					end
+					if !ftp.nlst.include?(File.basename(file))
+						ftp.mkdir(File.basename(file))
+					end
+				else
+					ftp.put(file, output.join("/"))
+				end
 			end
 		end
 		puts "-------------------------"
